109f73eaf136941b45fb29acd24abea1e57532ae
Wilbur <85022787+Pupper0n1@users.noreply.github.com>
2024-01-01 22:51:43 -0700

    Updated version

--- Backend/lib/openapi.py
+++ Backend/lib/openapi.py
@@ -4,7 +4,8 @@ from litestar.openapi.spec.contact import Contact
 config = OpenAPIConfig(
     title="Achevio API",
     version="0.2.0",
-    contact=Contact(name="Wilbur Elbouni", email="wilbur.elbouni@ucalgary.ca"),
+    description="Project Manager: Wilbur Elbouni",
+    # contact=Contact(name="Wilbur Elbouni", email="wilbur.elbouni@ucalgary.ca"),
     use_handler_docstrings=True,
 )
 """OpenAPI config for app."""

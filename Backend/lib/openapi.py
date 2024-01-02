3fc5afc6b14c0554d0a67373bbd1208ef99424b8
Wilbur <85022787+Pupper0n1@users.noreply.github.com>
2024-01-01 22:57:07 -0700

    Updated versionng

--- Backend/lib/openapi.py
+++ Backend/lib/openapi.py
@@ -1,11 +1,14 @@
 from litestar.openapi.config import OpenAPIConfig
 from litestar.openapi.spec.contact import Contact
+from litestar.openapi.spec.license import License
+
 
 config = OpenAPIConfig(
-    title="Achevio API",
-    version="0.2.0",
-    description="Project Manager: Wilbur Elbouni",
+    title='Achevio API',
+    version='0.2.0',
+    description='Project Manager: Wilbur Elbouni',
     # contact=Contact(name="Wilbur Elbouni", email="wilbur.elbouni@ucalgary.ca"),
     use_handler_docstrings=True,
+    license=License(name="This project is under the MIT License", url="https://opensource.org/licenses/MIT"),
 )
 """OpenAPI config for app."""

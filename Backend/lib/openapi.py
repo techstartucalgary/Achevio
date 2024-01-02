c9ce669a4503b7aaa6c3a8aba1d2505e74ea0f9c
Wilbur <85022787+Pupper0n1@users.noreply.github.com>
2024-01-01 22:43:35 -0700

    Updated version

--- Backend/lib/openapi.py
+++ Backend/lib/openapi.py
@@ -3,7 +3,7 @@ from litestar.openapi.spec.contact import Contact
 
 config = OpenAPIConfig(
     title="Achevio API",
-    version="0.1.1.1",
+    version="0.2.0",
     contact=Contact(name="Wilbur Elbouni", email="wilbur.elbouni@ucalgary.ca"),
     use_handler_docstrings=True,
 )

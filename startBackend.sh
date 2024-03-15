5cefd80b46185c7739f758dc82f1636cda88597f
Magdy-Hafez#030901 <113151015+MmagdyHafezZ@users.noreply.github.com>
2024-03-14 18:27:04 -0600

    bashscripts

---
+++ startBackend.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Attempt to fetch the private IP address
+# This command works on Linux and might work on other Unix-like systems.
+PRIVATE_IP=$(ip route get 1.2.3.4 | awk '{print $7}' | head -n 1)
+
+# Check if the IP address was successfully retrieved
+if [ -z "$PRIVATE_IP" ]; then
+  echo "Failed to fetch the private IP address."
+  exit 1
+fi
+
+# Assuming your Python virtual environment is located in "myenv"
+VENV_PATH="myenv"
+
+# Activate your virtual environment
+source "${VENV_PATH}/bin/activate"
+
+# Check if activation was successful
+if [ $? -ne 0 ]; then
+  echo "Failed to activate the virtual environment."
+  exit 1
+fi
+
+# Now run your litestar command directly
+cd Backend && litestar run -r -H ${PRIVATE_IP} --port 8002
+
+# The script exits after starting the litestar command.
+# If you run any commands after `litestar`, consider whether you need to deactivate the virtual environment.
---
+++ startFrontend.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Attempt to fetch the private IP address
+# This command works on Linux and might work on other Unix-like systems.
+PRIVATE_IP=$(ip route get 1.2.3.4 | awk '{print $7}' | head -n 1)
+
+# Check if the IP address was successfully retrieved
+if [ -z "$PRIVATE_IP" ]; then
+  echo "Failed to fetch the private IP address."
+  exit 1
+fi
+
+echo "Detected Private IP: $PRIVATE_IP"
+
+# Set the REACT_APP_PRIVATE_IP environment variable
+export REACT_APP_PRIVATE_IP=$PRIVATE_IP
+
+# Start the frontend in a new tmux session
+# Adjust the path as necessary to the root of your React project
+cd FrontEnd && REACT_APP_PRIVATE_IP=1234 npm start
+
+# To attach to the session, you can use: tmux attach-session -t frontend
+# To kill the session later, use: tmux kill-session -t frontend
+

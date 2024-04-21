76eac35715b9065c22db333daa1d77548be0c5ce
Magdy-Hafez#030901 <113151015+MmagdyHafezZ@users.noreply.github.com>
2024-04-20 20:47:49 -0600

    init

---
+++ FrontEnd/app/redux/actions.tsx
@@ -0,0 +1,41 @@
+export const SET_URL = "SET_URL";
+export const SET_USERNAME = "SET_USERNAME";
+export const SET_THEME = "SET_THEME";
+export const SET_USERID = "SET_USERID";
+export const SET_ACCESS_TOKEN = "SET_ACCESS_TOKEN";
+
+enum Theme {
+  Light = "light",
+  Dark = "dark",
+}
+
+export const setAccessToken = (accessToken: string) => (dispatch) => {
+  dispatch({
+    type: SET_ACCESS_TOKEN,
+    payload: accessToken,
+  });
+};
+export const setUrl = (url: string) => (dispatch) => {
+  dispatch({
+    type: SET_URL,
+    payload: url,
+  });
+};
+export const setUsername = (username: string) => (dispatch) => {
+  dispatch({
+    type: SET_USERNAME,
+    payload: username,
+  });
+};
+export const setTheme = (theme: Theme) => (dispatch) => {
+  dispatch({
+    type: SET_THEME,
+    payload: theme,
+  });
+};
+export const setUserId = (userId: string) => (dispatch) => {
+  dispatch({
+    type: SET_USERID,
+    payload: userId,
+  });
+};
